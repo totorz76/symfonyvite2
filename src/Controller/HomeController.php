d49c8540d7fca52bc1ad2dfb6222493736ab1f2a
JulienTouchard <julien.touchard@free.fr>
2026-02-20 09:51:25 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+
+        //var_dump($_SERVER);
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            
+        ]);
+    }
+}
